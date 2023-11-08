81c38ddba971e88a26b6cac11f12ecc8742e6224
Dylan <duanyytop@gmail.com>
2023-11-08 19:08:42 +0800

    connect with redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import * as React from "react";
 import {Address} from "viem";
-import {connect, disconnect, getConnectedAddress} from "@joyid/evm";
+import {connectCallback, connectWithRedirect, disconnect, getConnectedAddress} from "@joyid/evm";
 import "./App.css";
 
 export default function App() {
   const [address, setAddress] = React.useState<Address | null>(getConnectedAddress());
+
+  React.useEffect(() => {
+    try {
+      const res = connectCallback(window.location.href);
+      setAddress(res.address as Address);
+    } catch (error) {
+      //
+    }
+  }, [window.location.href]);
+
   const onConnect = async () => {
     try {
-      const res = await connect();
-      setAddress(res);
+      connectWithRedirect("https://joyid-bot.dev/");
     } catch (error) {
       console.log(error);
     }
