75e13f314b517d98688158182f010d88066bf932
Dylan <duanyytop@gmail.com>
2023-11-21 20:09:15 +0800

    fix build error

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {initConfig} from "@joyid/evm";
 import App from "./App";
-import "./index.css";
 
 const cacheTime = 1000 * 60 * 60 * 24;
 
