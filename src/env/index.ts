c746324e29ae90521304b9d3f5ecea6d23858e74
Dylan <duanyytop@gmail.com>
2024-01-22 14:33:26 +0800

    update env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,2 +1,2 @@
-export const CALLBACK_SERVER_URL = import.meta.env.VITE_APP_CALLBACK_SERVER_URL ?? "https://lucky-pocket-api.joyid.dev/api/v1";
-export const JOYID_APP_URL = import.meta.env.VITE_APP_JOYID_APP_URL ?? "https://joyid-app-git-mini-app-sign-tx-nervina.vercel.app/";
+export const CALLBACK_SERVER_URL = import.meta.env.VITE_APP_CALLBACK_SERVER_URL ?? "";
+export const JOYID_APP_URL = import.meta.env.VITE_APP_JOYID_APP_URL ?? "https://testnet.joyid.dev/";
